62f8dd36132029ef1a633b6af79fa44441c8107e
Seungjun Oh <osjuny11@gmail.com>
2022-05-28 14:48:27 +0900

    double quote 로 변경

--- my_app.py
+++ my_app.py
@@ -12,67 +12,67 @@ import streamlit as st
 #####
 # Use magic
 df = pd.DataFrame({
-	'first column': [1, 2, 3, 4],
-	'second column': [10, 20, 30, 40]
+	"first column": [1, 2, 3, 4],
+	"second column": [10, 20, 30, 40]
 })
 
-'Display using magic:'
+"Display using magic:"
 df
 
 #####
 # Write a data frame
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
-	'first column': [1, 2, 3, 4],
-	'second column': [10, 20, 30, 40]
+	"first column": [1, 2, 3, 4],
+	"second column": [10, 20, 30, 40]
 }))
 
 #####
 # Magic vs Write
 df = pd.DataFrame(
 	np.random.randn(10, 20),
-	columns=('col %d' % i for i in range(20))
+	columns=("col %d" % i for i in range(20))
 )
 
-st.write('Display dataframe with style:')
+st.write("Display dataframe with style:")
 st.dataframe(df.style.highlight_max(axis=0))
 
 #####
 # Draw line chart
 chart_data = pd.DataFrame(
 	np.random.randn(20, 3),
-	columns=['a', 'b', 'c']
+	columns=["a", "b", "c"]
 )
 
-st.write('Display dataframe to line chart:')
+st.write("Display dataframe to line chart:")
 st.line_chart(chart_data)
 
 #####
 # Plot a map
 map_data = pd.DataFrame(
 	np.random.randn(1000, 2) / [50, 50] + [37.5, 127.0],
-	columns=['lat', 'lon']
+	columns=["lat", "lon"]
 )
 
-st.write('Display dataframe to map:')
+st.write("Display dataframe to map:")
 st.map(map_data)
 
 #####
 # Simple Widget
-st.write('Slider Widget:')
-x = st.slider('x')
-st.write(x, 'squared is', x * x)
+st.write("Slider Widget:")
+x = st.slider("x")
+st.write(x, "squared is", x * x)
 
-st.write('Text Input Widget:')
+st.write("Text Input Widget:")
 st.text_input("Your name", key="name")
-st.write(f'Hello, {st.session_state.name}')
+st.write(f"Hello, {st.session_state.name}")
 
 #####
 # Use checkboxes to show/hide data
-if st.checkbox('Show dataframe'):
+if st.checkbox("Show dataframe"):
 	chart_data = pd.DataFrame(
 		np.random.randn(20, 3),
-		columns=['a', 'b', 'c']
+		columns=["a", "b", "c"]
 	)
 
 	chart_data
@@ -80,44 +80,44 @@ if st.checkbox('Show dataframe'):
 #####
 # Use a selectbox for options
 df = pd.DataFrame({
-	'first column': [1, 2, 3, 4],
-	'second column': [10, 20, 30, 40]
+	"first column": [1, 2, 3, 4],
+	"second column": [10, 20, 30, 40]
 })
 
 option = st.selectbox(
-	'Which number do you like best?',
-	df['first column']
+	"Which number do you like best?",
+	df["first column"]
 )
-st.write(f'You selected: {option}')
+st.write(f"You selected: {option}")
 
 #####
 # Sidebar Layout
 add_selectbox = st.sidebar.selectbox(
-	'How would you like to be contacted?',
-	('Email', 'Home phone', 'Mobile phone')
+	"How would you like to be contacted?",
+	("Email", "Home phone", "Mobile phone")
 )
 
 # Add a slider to the sidebar:
 add_slider = st.sidebar.slider(
-	'Select a range of values',
+	"Select a range of values",
 	0.0, 100.0, (25.0, 75.0)
 )
 
 #####
 # Column Layout
 left_column, right_column = st.columns(2)
-left_column.button('Press me!')
+left_column.button("Press me!")
 
 with right_column:
 	chosen = st.radio(
-		'Sorting hat',
+		"Sorting hat",
 		("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
 	st.write(f"You are in {chosen} house!")
 
 #####
 # Show Progress
 
-'Starting a long computation...'
+"Starting a long computation..."
 
 # Add a placeholder
 latest_iteration = st.empty()
@@ -125,8 +125,8 @@ bar = st.progress(0)
 
 for i in range(10):
 	# Update the progress bar with each iteration.
-	latest_iteration.text(f'Iteration {i + 1}')
+	latest_iteration.text(f"Iteration {i + 1}")
 	bar.progress((i + 1) * 10)
 	time.sleep(0.1)
 
-'...and now we\'re done!'
+"...and now we\"re done!"
